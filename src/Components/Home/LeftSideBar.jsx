a6fe56b2448cc0321b56c322dcdede8d14816106
Pratyay Bandyopadhyay <praty94@gmail.com>
2020-11-29 19:31:07 +0530

    Code cleanup

--- src/Components/Home/LeftSideBar.jsx
+++ src/Components/Home/LeftSideBar.jsx
@@ -5,77 +5,52 @@ import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
 import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import Avatar from '@material-ui/core/Avatar';
-import { Flag, Storefront,VideoLibrary } from '@material-ui/icons';
+import { Flag, Storefront, VideoLibrary } from '@material-ui/icons';
 import HomeIcon from '@material-ui/icons/HomeRounded';
 import GroupIcon from '@material-ui/icons/SupervisedUserCircleSharp';
 import AvatarWithBadge from '../AvatarWithBadge/AvatarWithBadge';
-import {themeColors} from '../../Theme/ThemeHelper';
+import { themeColors } from '../../Theme/ThemeHelper';
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
     maxWidth: 360,
-    color:themeColors[theme.palette.type].text
+    color: themeColors[theme.palette.type].text
   },
-  sidebarIcon:{   
+  sidebarIcon: {
     backgroundColor: themeColors[theme.palette.type].background,
-    color:'inherit'
+    color: 'inherit'
   },
-  listItem:{
-    borderRadius:10,
-    '&:hover':{
-      background:themeColors[theme.palette.type].background
+  listItem: {
+    borderRadius: 10,
+    '&:hover': {
+      background: themeColors[theme.palette.type].background
     }
   }
 }));
 
 export default function LeftSideBar() {
   const classes = useStyles();
-
+  const leftBarItems = [{ title: "Home", icon: <HomeIcon /> },
+  { title: "Pages", icon: <Flag /> },
+  { title: "Videos", icon: <VideoLibrary /> },
+  { title: "Marketplace", icon: <Storefront /> },
+  { title: "Groups", icon: <GroupIcon /> }]
+  
   return (
     <List className={classes.root}>
       <ListItem button className={classes.listItem}>
-      <AvatarWithBadge fullV></AvatarWithBadge>
+        <AvatarWithBadge fullV></AvatarWithBadge>
       </ListItem>
-      <ListItem button className={classes.listItem}>
-        <ListItemAvatar>
-          <Avatar className={classes.sidebarIcon}>
-            <HomeIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Home"/>
-      </ListItem>
-      <ListItem button className={classes.listItem}>
-        <ListItemAvatar>
-          <Avatar className={classes.sidebarIcon}>
-            <Flag />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Pages"/>
-      </ListItem>
-      <ListItem button className={classes.listItem}>
-        <ListItemAvatar>
-          <Avatar className={classes.sidebarIcon}>
-            <VideoLibrary />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Videos" />
-      </ListItem>
-      <ListItem button className={classes.listItem}>
-        <ListItemAvatar>
-          <Avatar className={classes.sidebarIcon}>
-            <Storefront />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Marketplace" />
-      </ListItem>
-      <ListItem button className={classes.listItem}>
-        <ListItemAvatar>
-          <Avatar className={classes.sidebarIcon}>
-            <GroupIcon />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Groups" />
-      </ListItem>
+      {leftBarItems.map((item, key) => {
+        return (<ListItem button key={key} className={classes.listItem}>
+          <ListItemAvatar>
+            <Avatar className={classes.sidebarIcon}>
+              {item.icon}
+            </Avatar>
+          </ListItemAvatar>
+          <ListItemText primary={item.title} />
+        </ListItem>);
+      })}
     </List>
   );
 }
